6101880e1f39d360fbad6b9a0171ef6b3a6a9943
YaryyGam <gamerplay0606@gmail.com>
2025-01-07 22:10:17 +0200

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
---INSERT INTO student (name, age) VALUES ('Pablo', 22);
---INSERT INTO student (name, age) VALUES ('Pedro', 25);
---INSERT INTO student (name, age) VALUES ('Hylio', 222);
---INSERT INTO student (name, age) VALUES ('Jmih', 12);
+INSERT INTO student (name, age) VALUES ('Pablo', 22);
+INSERT INTO student (name, age) VALUES ('Pedro', 25);
+INSERT INTO student (name, age) VALUES ('Hylio', 222);
+INSERT INTO student (name, age) VALUES ('Jmih', 12);
